e8633d5fabf04fc88615cac94f54bfd8b9f2d812
mocozairos <mcruzdelirafilho@gmail.com>
2024-10-22 13:25:08 -0300

    Update roteirizador_nat.py

--- roteirizador_nat.py
+++ roteirizador_nat.py
@@ -4764,6 +4764,8 @@ if servico_roteiro and 'df_horario_esp_ultimo_hotel' in st.session_state:
 
         st.dataframe(st.session_state.df_horario_esp_ultimo_hotel, hide_index=True) 
 
+st.session_state.df_natal
+
 # Roteirizando Regiões
 
 if roteirizar:
